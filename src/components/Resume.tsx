a8ea8715c148afdb8f9ad489c93fb17d71d420f7
MaxKeable <max.keable488@gmail.com>
2023-08-18 09:39:21 +1000

    fixed pdf download

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -6,37 +6,44 @@ import { SlideReveal } from "./animations/SlideReveal";
 import { Element } from "react-scroll";
 
 const Resume = () => {
+  const onDownloadPDFClick = () => {
+    fetch("Resume.pdf").then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "Resume.pdf";
+        alink.click();
+      });
+    });
+  };
+
   return (
     <Element name="resumeSection">
       <Box display="flex" flexWrap="wrap" mb={4}>
         <Box className="tiles">
           <SlideReveal delay={0.3}>
-            <a
-              href={`${process.env.PUBLIC_URL}/resume.pdf`}
-              download="Resume"
-              style={{ textDecoration: "none" }}
+            <Box
+              onClick={onDownloadPDFClick}
+              sx={{
+                backgroundColor: `rgba(255, 255, 255, 0.2)`,
+                height: "200px",
+                width: "200px",
+                borderRadius: "15px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                ml: 2,
+                mt: 2,
+              }}
             >
-              <Box
-                sx={{
-                  backgroundColor: `rgba(255, 255, 255, 0.2)`,
-                  height: "200px",
-                  width: "200px",
-                  borderRadius: "15px",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  ml: 2,
-                  mt: 2,
-                }}
-              >
-                <Typography variant="h5" sx={{ mt: 1 }}>
-                  PDF Download
-                </Typography>
-                <SimCardDownloadIcon
-                  sx={{ fontSize: "9em", mt: 1, color: "white" }}
-                />
-              </Box>
-            </a>
+              <Typography variant="h5" sx={{ mt: 1 }}>
+                PDF Download
+              </Typography>
+              <SimCardDownloadIcon
+                sx={{ fontSize: "9em", mt: 1, color: "white" }}
+              />
+            </Box>
           </SlideReveal>
         </Box>
         <Box className="tiles">
